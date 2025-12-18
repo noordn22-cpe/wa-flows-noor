08458499f3a09c5cac6ca7ec08857bc396806e71
noordn22-cpe <noordn22@gmail.com>
2025-12-18 09:33:16 +0530

    Update workflow_failure.py

--- workflow_failure.py
+++ workflow_failure.py
@@ -1,13 +1,10 @@
 from prefect import flow, task
- 
-@task
-def failing_task():
-   raise Exception("Simulated failure")
- 
-@flow
-def failure_workflow():
-  failing_task()
- 
+
 @task(retries=3, retry_delay_seconds=5)
 def retry_task():
-   raise Exception("Retrying")
+    print("Task attempt")
+    raise Exception("Retrying")
+
+@flow
+def failure_workflow():
+    retry_task()
